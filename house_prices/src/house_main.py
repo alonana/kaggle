304b0fd71d6c190c929a9b32809e887e36b7d671
Alon Tamir <alonta@radware.com>
2019-03-19 21:51:27 +0200

    house prices - add categories

--- house_prices/src/house_main.py
+++ house_prices/src/house_main.py
@@ -3,9 +3,11 @@ import pickle
 from datetime import datetime
 from math import log
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib import rcParams
+from matplotlib.ticker import MaxNLocator
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
@@ -14,7 +16,7 @@ COL_ID = 'passengerid'
 
 TEST_PATH = '../data/test.csv'
 TRAIN_PATH = '../data/train.csv'
-MODEL_PATH = '../model/{}.dat'
+MODEL_PATH = '../output/model/{}.dat'
 SUBMISSION_PATH = '../output/my_submission.csv'
 
 SEED = 42
@@ -28,28 +30,16 @@ class House:
 
     def __init__(self):
         self.d = pd.read_csv(TRAIN_PATH)
+        self.dp = self.prepare_data(self.d, False)
+        self.numeric_columns = self.dp._get_numeric_data().columns
+        self.categoric_columns = list(set(self.dp.columns) - set(self.numeric_columns))
+        self.prepared_columns = {}
         pd.set_option('display.max_columns', 500)
-        pd.set_option('display.width', 1000)
+        pd.set_option('display.width', 10000)
+        pd.set_option('display.float_format', lambda x: '%.6f' % x)
         rcParams.update({'figure.autolayout': True})
 
-    def general(self):
-        debug("general")
-
-        with open("../output/general/head.txt", "w") as file:
-            file.write(str(self.d.head()))
-
-        with open("../output/general/desc.txt", "w") as file:
-            file.write(str(self.d.describe()))
-
-        with open("../output/general/desc_objects.txt", "w") as file:
-            file.write(str(self.d.describe(include='O')))
-
-        buf = io.StringIO()
-        self.d.info(buf=buf)
-        with open("../output/general/info.txt", "w") as file:
-            file.write(buf.getvalue())
-
-    def prepare_data(self, d):
+    def prepare_data(self, d, final_data=True, suffix=''):
         x = d.copy()
         x.columns = [n.lower() for n in x.columns]
 
@@ -57,13 +47,46 @@ class House:
             x[COL_Y] = x.saleprice.apply(lambda c: log(c, 10))
             x.drop('saleprice', axis=1, inplace=True)
 
-        x = x._get_numeric_data()
-        x.fillna(x.mean(), inplace=True)
-        debug(x.head())
+        if final_data:
+            all = x.copy()
+            x = x._get_numeric_data()
+            x = x.join(all[self.categoric_columns])
+            x = pd.get_dummies(x)
+
+            non_common_columns = ['condition2_RRAe', 'miscfeature_TenC', 'housestyle_2.5Fin', 'exterior1st_Stone',
+                                  'electrical_Mix', 'exterior2nd_Other', 'condition2_RRNn', 'roofmatl_Metal',
+                                  'exterior1st_ImStucc', 'roofmatl_Roll', 'heating_OthW', 'roofmatl_ClyTile',
+                                  'poolqc_Fa', 'heating_Floor', 'condition2_RRAn', 'utilities_NoSeWa',
+                                  'roofmatl_Membran', 'garagequal_Ex']
+            for c in non_common_columns:
+                if c in x:
+                    x.drop(c, axis=1, inplace=True)
+
+            x.fillna(x.mean(), inplace=True)
+            self.prepared_columns[suffix] = list(x)
+            with open("../output/text/prepared_data_{}.txt".format(suffix), "w") as file:
+                file.write(str(x))
         return x
 
+    def general(self):
+        debug("general")
+
+        with open("../output/text/head.txt", "w") as file:
+            file.write(str(self.d.head()))
+
+        with open("../output/text/desc.txt", "w") as file:
+            file.write(str(self.d.describe()))
+
+        with open("../output/text/desc_objects.txt", "w") as file:
+            file.write(str(self.d.describe(include='O')))
+
+        buf = io.StringIO()
+        self.d.info(buf=buf)
+        with open("../output/text/info.txt", "w") as file:
+            file.write(buf.getvalue())
+
     def train_model(self, name, model, test_size=0.3):
-        prepared = self.prepare_data(self.d)
+        prepared = self.prepare_data(self.d, suffix="train")
 
         debug("splitting")
         prepared = prepared.reindex(np.random.RandomState(seed=SEED).permutation(prepared.index))
@@ -83,17 +106,30 @@ class House:
             if hasattr(model, 'feature_importances_'):
                 importance = pd.DataFrame(model.feature_importances_, index=prepared.columns, columns=['importance'])
                 importance = importance.sort_values('importance', ascending=False)
-                debug("importance {}".format(importance))
+                with open("../output/text/importance.txt", "w") as file:
+                    file.write(str(importance))
 
         with open(MODEL_PATH.format(name), 'wb') as f:
             pickle.dump(model, f)
+
+    def prepared_columns_diff(self):
+        debug('columns diff')
+        train_cols = self.prepared_columns["train"]
+        predict_cols = self.prepared_columns["predict"]
+
+        diff = set(train_cols).symmetric_difference(set(predict_cols))
+        if len(diff) > 0:
+            debug("mismatching columns located")
+            debug(diff)
 
     def predict_submission(self, name):
         debug("predict start")
         with open(MODEL_PATH.format(name), 'rb') as f:
             model = pickle.load(f)
         test = pd.read_csv(TEST_PATH)
-        prepared = self.prepare_data(test)
+        prepared = self.prepare_data(test, suffix="predict")
+
+        self.prepared_columns_diff()
 
         predictions = model.predict(prepared)
         debug(predictions)
@@ -103,9 +139,27 @@ class House:
         submission.index += 1461
         submission.to_csv(SUBMISSION_PATH, header=True, index=True)
 
+    def hist_per_category(self, df, category, column, prefix="", bins=30):
+        fig, ax = plt.subplots()
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+        category_values = df[category].unique()
+        plt.hist([df.loc[df[category] == y, column] for y in category_values], label=category_values, bins=bins)
+        ax.legend()
+        plt.title("{} histogram by {}".format(column, category))
+
+        fig.savefig("../output/graph/{}hist_{}_per_{}.png".format(prefix, column, category))
+        plt.close()
+
+    def hist_for_categories(self):
+        for c in self.categoric_columns:
+            self.hist_per_category(self.dp, c, COL_Y)
+
 
 h = House()
-# h.general()
+h.general()
+# h.hist_for_categories()
 model = RandomForestRegressor(n_estimators=100)
 classifier_name = "random_forest"
 h.train_model("%s" % classifier_name, model)
