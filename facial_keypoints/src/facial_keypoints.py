2c20a4529e7b57692695778670e77704f01ccfbb
Alon Tamir <alonta@radware.com>
2019-05-11 14:20:01 +0300

    facial

--- facial_keypoints/src/facial_keypoints.py
+++ facial_keypoints/src/facial_keypoints.py
@@ -10,9 +10,11 @@ from tensorflow.python.keras.models import Sequential
 
 TRAIN_PATH = '../data/training.csv'
 TEST_PATH = '../data/test.csv'
+LOOKUP_PATH = '../data/IdLookupTable.csv'
 OUTPUT_PATH = '../output'
 IMAGES_PATH = "{}/images".format(OUTPUT_PATH)
 MODEL_PATH = "{}/model.dat".format(OUTPUT_PATH)
+PREDICTIONS_PATH = '{}/predictions.csv'.format(OUTPUT_PATH)
 IMAGE_SIZE = 96
 NUM_CLASSES = 2
 
@@ -24,22 +26,40 @@ def debug(msg):
 class Facial:
     def __init__(self, path=TRAIN_PATH, max_rows=None, train_mode=True):
         self.train_mode = train_mode
-        self.target_labels = ['left_eye_center_x', 'left_eye_center_y']
+        self.target_labels = []
+        self.build_target_labels()
+
         pathlib.Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
         pathlib.Path(IMAGES_PATH).mkdir(parents=True, exist_ok=True)
         debug("loading csv from {} starting".format(path))
         self.df = pd.read_csv(path, nrows=max_rows)
         debug("loading csv done, loaded {} rows".format(self.df.shape[0]))
 
+    def build_target_labels(self):
+        bilateral = ['DDD_eye_center', 'DDD_eye_inner_corner', 'DDD_eye_outer_corner', 'DDD_eyebrow_inner_end',
+                     'DDD_eyebrow_outer_end', 'mouth_DDD_corner']
+
+        for n in bilateral:
+            self.add_target_label(n.replace("DDD", "left"))
+            self.add_target_label(n.replace("DDD", "right"))
+
+        for n in ['nose_tip', 'mouth_center_top_lip', 'mouth_center_bottom_lip']:
+            self.add_target_label(n)
+
+    def add_target_label(self, name):
+        self.target_labels += ['{}_x'.format(name), '{}_y'.format(name)]
+
     def save_images(self):
         for i, row in self.df.iterrows():
             pixels = self.get_pixels(row)
             image = np.reshape(pixels, (IMAGE_SIZE, IMAGE_SIZE))
             plt.imsave("{}/{}.png".format(IMAGES_PATH, i), image)
-            left_eye_center_x = int(row['left_eye_center_x'])
-            left_eye_center_y = int(row['left_eye_center_y'])
-            image[left_eye_center_y][left_eye_center_x] = 255
-            plt.imsave("{}/{}_left_eye.png".format(IMAGES_PATH, i), image)
+            labels_iter = iter(self.target_labels)
+            for x_label, y_label in zip(labels_iter, labels_iter):
+                x = int(row[x_label])
+                y = int(row[y_label])
+                image[y][x] = 255
+            plt.imsave("{}/{}_points.png".format(IMAGES_PATH, i), image)
 
     def get_pixels(self, row):
         pixels = [int(x) for x in row['Image'].split()]
@@ -110,7 +130,25 @@ class Facial:
         x, y = self.data_prepare()
 
         predictions = model.predict(x).tolist()
-        debug(predictions)
+        index = [x + 1 for x in range(len(predictions))]
+        predictions_df = pd.DataFrame(predictions, columns=self.target_labels, index=index)
+        predictions_df.index.name = 'Index'
+        predictions_df.to_csv(PREDICTIONS_PATH)
+
+    def submit(self):
+        predictions = pd.read_csv(PREDICTIONS_PATH, dtype={'Index': str}, index_col='Index')
+        for i, r in predictions.iterrows():
+            print(r)
+
+        print(predictions.head())
+        lookup = pd.read_csv(LOOKUP_PATH)
+        for i, row in lookup.iterrows():
+            print(row)
+            image = str(row['ImageId'])
+            label = row['FeatureName']
+            prediction_row = predictions.loc[image]
+            location = prediction_row[label]
+            print(location)
 
     def display_missing_values(self):
         all_data_na = (self.df.isnull().sum() / len(self.df)) * 100
@@ -120,7 +158,7 @@ class Facial:
 
 
 def train():
-    f = Facial(max_rows=100)
+    f = Facial(max_rows=10)
     # f.display_missing_values()
     # f.save_images()
     f.build_model()
@@ -128,8 +166,9 @@ def train():
 
 def predict():
     f = Facial(max_rows=2, path=TEST_PATH, train_mode=False)
-    f.predict()
+    # f.predict()
+    f.submit()
 
 
-train()
-# predict()
+# train()
+predict()
