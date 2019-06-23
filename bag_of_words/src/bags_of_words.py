48fd63e1abb43cc5c59b670c667514943bf83636
Alon Tamir <alonta@radware.com>
2019-06-23 18:46:15 +0300

    bags

--- bag_of_words/src/bags_of_words.py
+++ bag_of_words/src/bags_of_words.py
@@ -13,9 +13,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
 COL_Y = 'sentiment'
-
+TRAIN_DATA = "../data/labeledTrainData.tsv"
+TEST_DATA = "../data/testData.tsv"
+MODEL_DAT = "../output/model.dat"
 PREPARED_CSV = "../output/prepared.csv"
-CLEAN_CSV = "../output/clean.csv"
+TRAIN_CLEAN_DATA = "../output/train_clean.csv"
+TEST_CLEAN_DATA = "../output/test_clean.csv"
+VECTORIZER = "../output/vectorizer.dat"
 
 
 def debug(msg, new_line=False):
@@ -27,11 +31,12 @@ def debug(msg, new_line=False):
 
 class MyBag:
 
-    def __init__(self, scratch=False):
+    def __init__(self, scratch=False, calculation_limit_rows=None):
         if scratch:
             nltk.download('stopwords')
         pathlib.Path("../output").mkdir(parents=True, exist_ok=True)
         self.cleaned_rows = 0
+        self.calculation_limit_rows = calculation_limit_rows
 
     def clean_column(self, text):
         clean = BeautifulSoup(text, features="html.parser").get_text()
@@ -45,19 +50,26 @@ class MyBag:
             debug("cleaned {} rows".format(self.cleaned_rows))
         return result
 
-    def clean_data(self):
-        df = pd.read_csv("../data/labeledTrainData.tsv", delimiter='\t', header=0, quoting=3)
+    def clean_data(self, input_file_name, output_file_name):
+        self.cleaned_rows = 0
+        debug("clean data from {} to {}".format(input_file_name, output_file_name))
+        df = pd.read_csv(input_file_name, delimiter='\t', header=0, quoting=3, nrows=self.calculation_limit_rows)
         df.review = df.review.apply(self.clean_column)
-        df.to_csv(CLEAN_CSV)
+        df.to_csv(output_file_name)
 
     def prepare_data(self):
-        df = pd.read_csv(CLEAN_CSV)
+        debug("preparing data")
+        df = pd.read_csv(TRAIN_CLEAN_DATA, nrows=self.calculation_limit_rows)
         debug(df.head(), new_line=True)
 
         vectorizer = CountVectorizer(analyzer="word", tokenizer=None, preprocessor=None, stop_words=None,
                                      max_features=5000)
 
         features = vectorizer.fit_transform(df.review)
+
+        with open(VECTORIZER, 'wb') as f:
+            pickle.dump(vectorizer, f)
+
         features = features.toarray()
         features_names = vectorizer.get_feature_names()
         amounts = np.sum(features, axis=0)
@@ -72,28 +84,51 @@ class MyBag:
         debug(prepared.head(), new_line=True)
         prepared.to_csv(PREPARED_CSV)
 
-    def train(self):
-        debug("read prepared data")
-        df = pd.read_csv(PREPARED_CSV)
+    def train(self, test_size=0.3):
+        debug("train starting - read prepared data")
+        df = pd.read_csv(PREPARED_CSV, nrows=self.calculation_limit_rows)
         df.drop("Unnamed: 0", axis=1, inplace=True)
         debug(df.head(), new_line=True)
         model = RandomForestClassifier(n_estimators=100, verbose=1)
         y = np.array(df[COL_Y])
         df = df.drop(COL_Y, 1)
 
-        X = np.array(df)
-        train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
+        x = np.array(df)
+        train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=test_size)
         model.fit(train_x, train_y)
 
-        score = model.score(test_x, test_y)
-        debug("model score: {}".format(score))
+        if test_size > 0:
+            score = model.score(test_x, test_y)
+            debug("model score: {}".format(score))
         if hasattr(model, 'feature_importances_'):
             importance = pd.DataFrame(model.feature_importances_, index=df.columns, columns=['importance'])
             importance = importance.sort_values('importance', ascending=False)
             with open("../output/importance.txt", "w") as file:
                 file.write(str(importance))
-        with open("../output/model.dat", 'wb') as f:
+        with open(MODEL_DAT, 'wb') as f:
             pickle.dump(model, f)
+
+    def predict(self):
+        debug("predict start")
+        with open(VECTORIZER, 'rb') as f:
+            vectorizer = pickle.load(f)
+
+        df = pd.read_csv(TEST_CLEAN_DATA, nrows=self.calculation_limit_rows)
+        df.drop("Unnamed: 0", axis=1, inplace=True)
+        debug(df.head(), new_line=True)
+
+        features = vectorizer.transform(df.review)
+        features = features.toarray()
+
+        debug("predict features {}".format(features))
+        with open(MODEL_DAT, 'rb') as f:
+            model = pickle.load(f)
+
+        predictions = model.predict(features)
+
+        df.drop('review', axis=1, inplace=True)
+        df[COL_Y] = predictions
+        df.to_csv("../output/submission.csv", index=False, quoting=3)
 
 
 pd.set_option('display.max_rows', 5000)
@@ -101,6 +136,9 @@ pd.set_option('display.max_columns', 5000)
 pd.set_option('display.width', 100000)
 
 b = MyBag()
-# b.clean_data()
-# b.prepare_data()
-b.train()
+b.clean_data(TRAIN_DATA, TRAIN_CLEAN_DATA)
+b.prepare_data()
+b.train(test_size=0)
+
+b.clean_data(TEST_DATA, TEST_CLEAN_DATA)
+b.predict()
