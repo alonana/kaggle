96b3ae6c5da833680c7f12961d943d152f100254
Alon Tamir <alonta@radware.com>
2019-07-04 21:45:46 +0300

    pets

--- pet/src/pet_main.py
+++ pet/src/pet_main.py
@@ -3,7 +3,12 @@ from datetime import datetime
 
 import pandas as pd
 
-TRAIN_DATA = "../data/train.csv"
+DATA_FOLDER = "../data"
+TRAIN_DATA = "%s/train.csv" % DATA_FOLDER
+BREED_LABELS = "%s/breed_labels.csv" % DATA_FOLDER
+COLOR_LABELS = "%s/color_labels.csv" % DATA_FOLDER
+
+NA = 'N/A'
 
 
 def debug(msg, new_line=False):
@@ -17,12 +22,58 @@ class Pets:
     def __init__(self, calculation_limit_rows=None):
         pathlib.Path("../output").mkdir(parents=True, exist_ok=True)
         self.calculation_limit_rows = calculation_limit_rows
+        self.df = pd.read_csv(TRAIN_DATA, nrows=self.calculation_limit_rows)
 
     def pre_process(self):
-        df = pd.read_csv(TRAIN_DATA, nrows=self.calculation_limit_rows)
-        df['Type'] = df.apply(lambda row: 'dog' if row['Type'] == 1 else 'cat' if row['Type'] == 2 else None, axis=1)
-        df.rename(columns={'Age': 'AgeMonths'}, inplace=True)
-        debug(df.head(), new_line=True)
+        breeds = self.load_breeds()
+        colors = self.load_colors()
+        yes_no_not_sure = {
+            1: 'Yes',
+            2: 'No',
+            3: NA,
+        }
+        self.translate_column('Type', {1: 'dog', 2: 'cat'})
+        self.df.rename(columns={'Age': 'AgeMonths'}, inplace=True)
+        self.translate_column('Breed1', breeds)
+        self.translate_column('Breed2', breeds)
+        self.translate_column('Gender', {1: 'Male', 2: 'Female', 3: 'Mixed'})
+        self.translate_column('Color1', colors)
+        self.translate_column('Color2', colors)
+        self.translate_column('Color3', colors)
+        self.translate_column('MaturitySize', {0: NA, 1: 'Small', 2: 'Medium', 3: 'Large', 4: 'ExtraLarge'})
+        self.translate_column('FurLength', {0: NA, 1: 'Short', 2: 'Medium', 3: 'Long'})
+        self.translate_column('Vaccinated', yes_no_not_sure)
+        self.translate_column('Dewormed', yes_no_not_sure)
+        self.translate_column('Sterilized', yes_no_not_sure)
+        self.translate_column('Health', {0: NA, 1: 'Healthy', 2: 'Minor Injury', 3: 'Serious Injury'})
+        debug(self.df.head(self.calculation_limit_rows), new_line=True)
+
+    def translate_column(self, column, translation):
+        self.df[column] = self.df.apply(lambda row: self.produce_translation(row, column, translation), axis=1)
+
+    def produce_translation(self, row, column, translation):
+        v = row[column]
+        if v in translation:
+            return translation[v]
+        raise Exception('column {} has unexpected value {}'.format(column, v))
+
+    def load_breeds(self):
+        breeds_data = pd.read_csv(BREED_LABELS)
+        breeds = {
+            0: NA,
+        }
+        for i, row in breeds_data.iterrows():
+            breeds[row['BreedID']] = row['BreedName']
+        return breeds
+
+    def load_colors(self):
+        colors_data = pd.read_csv(COLOR_LABELS)
+        colors = {
+            0: NA,
+        }
+        for i, row in colors_data.iterrows():
+            colors[row['ColorID']] = row['ColorName']
+        return colors
 
 
 pd.set_option('display.max_rows', 5000)
