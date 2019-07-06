2fbfd8f7b169a73ec7d38027c2ece79d93d3e558
Alon Tamir <alonta@radware.com>
2019-07-06 14:29:52 +0300

    pets

--- pet/src/pet_main.py
+++ pet/src/pet_main.py
@@ -1,12 +1,24 @@
 import pathlib
+import pickle
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
+OUTPUT_FOLDER = "../output/"
 DATA_FOLDER = "../data"
+MODEL_PATH = "%s/model.dat" % OUTPUT_FOLDER
 TRAIN_DATA = "%s/train.csv" % DATA_FOLDER
 BREED_LABELS = "%s/breed_labels.csv" % DATA_FOLDER
 COLOR_LABELS = "%s/color_labels.csv" % DATA_FOLDER
+STATE_LABELS = "%s/state_labels.csv" % DATA_FOLDER
+
+COL_Y = 'AdoptionSpeed'
+SEED = 42
 
 NA = 'N/A'
 
@@ -20,11 +32,17 @@ def debug(msg, new_line=False):
 
 class Pets:
     def __init__(self, calculation_limit_rows=None):
-        pathlib.Path("../output").mkdir(parents=True, exist_ok=True)
+        pathlib.Path(OUTPUT_FOLDER).mkdir(parents=True, exist_ok=True)
         self.calculation_limit_rows = calculation_limit_rows
+        if calculation_limit_rows:
+            self.head_lines = calculation_limit_rows
+        else:
+            self.head_lines = 5
         self.df = pd.read_csv(TRAIN_DATA, nrows=self.calculation_limit_rows)
 
     def pre_process(self):
+        debug('pre process')
+        states = self.load_states()
         breeds = self.load_breeds()
         colors = self.load_colors()
         yes_no_not_sure = {
@@ -46,7 +64,8 @@ class Pets:
         self.translate_column('Dewormed', yes_no_not_sure)
         self.translate_column('Sterilized', yes_no_not_sure)
         self.translate_column('Health', {0: NA, 1: 'Healthy', 2: 'Minor Injury', 3: 'Serious Injury'})
-        debug(self.df.head(self.calculation_limit_rows), new_line=True)
+        self.translate_column('State', states)
+        debug(self.df.head(self.head_lines), new_line=True)
 
     def translate_column(self, column, translation):
         self.df[column] = self.df.apply(lambda row: self.produce_translation(row, column, translation), axis=1)
@@ -75,10 +94,84 @@ class Pets:
             colors[row['ColorID']] = row['ColorName']
         return colors
 
+    def load_states(self):
+        states_data = pd.read_csv(STATE_LABELS)
+        states = {
+            0: NA,
+        }
+        for i, row in states_data.iterrows():
+            states[row['StateID']] = row['StateName']
+        return states
+
+    def display_missing_values(self):
+        debug('analyze missing values')
+        all_data_na = (self.df.isnull().sum() / len(self.df)) * 100
+        all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)
+        missing_data = pd.DataFrame({'Missing Ratio': all_data_na})
+        with open(OUTPUT_FOLDER + "/missing_values.txt", "w") as file:
+            file.write(str(missing_data))
+
+        if all_data_na.size > 0:
+            debug("Missing values located!!")
+            fig, ax = plt.subplots(figsize=(15, 12))
+            plt.xticks(rotation='90')
+            sns.barplot(x=all_data_na.index, y=all_data_na)
+            plt.xlabel('Features', fontsize=15)
+            plt.ylabel('Percent of missing values', fontsize=15)
+            plt.title('Percent missing data by feature', fontsize=15)
+            fig.savefig(OUTPUT_FOLDER + "/missing_values.png")
+
+    def prepare_data(self):
+        self.pre_process()
+        debug('prepare data')
+        # debug (self.df.info())
+        # self.display_missing_values()
+        self.df.drop('Name', axis=1, inplace=True)
+        self.df.drop('RescuerID', axis=1, inplace=True)
+        self.df.drop('Description', axis=1, inplace=True)
+        self.df.drop('PetID', axis=1, inplace=True)
+        self.df = pd.get_dummies(self.df)
+        debug(self.df.head(self.head_lines), new_line=True)
+
+    def train_model(self, test_size=0.3):
+        self.prepare_data()
+
+        debug('train model')
+        model = RandomForestClassifier(n_estimators=800,
+                                       min_samples_split=2,
+                                       min_samples_leaf=1,
+                                       max_features='sqrt',
+                                       max_depth=100,
+                                       bootstrap=False,
+                                       verbose=10,
+                                       random_state=SEED)
+
+        y = np.array(self.df[COL_Y])
+        prepared = self.df.drop(COL_Y, 1)
+
+        x = np.array(prepared)
+        train_x, test_x, train_y, test_y = \
+            train_test_split(x, y, test_size=test_size, random_state=SEED)
+
+        model.fit(train_x, train_y)
+
+        if test_size > 0:
+            score = model.score(test_x, test_y)
+            debug("model score: {}".format(score))
+            if hasattr(model, 'feature_importances_'):
+                importance = pd.DataFrame(model.feature_importances_, index=prepared.columns, columns=['importance'])
+                importance = importance.sort_values('importance', ascending=False)
+
+                with open(OUTPUT_FOLDER + "importance.txt", "w") as file:
+                    file.write(str(importance))
+
+        with open(MODEL_PATH, 'wb') as f:
+            pickle.dump(model, f)
+
 
 pd.set_option('display.max_rows', 5000)
 pd.set_option('display.max_columns', 5000)
 pd.set_option('display.width', 100000)
 
-p = Pets(calculation_limit_rows=10)
-p.pre_process()
+p = Pets(calculation_limit_rows=None)
+p.train_model()
